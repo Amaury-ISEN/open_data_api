d4fe24d03a0311263f45075f8c6b292b1477736f
Amaury Bonneau <amaury.bonneau@isen-ouest.yncrea.fr>
2021-02-04 10:49:01 +0100

    ajout d'appli flask dans test.py avec base.html et index.html dans templates

--- api.py
+++ api.py
@@ -3,30 +3,55 @@ from fastapi import FastAPI
 import uvicorn # ASGI server
 from data import DataAccess as da
 from fastapi.encoders import jsonable_encoder
+from urllib.parse import urlparse
 
 # from models import Item
 
-app = FastAPI()
+app = FastAPI(redoc_url=None)
 
 @app.get("/")
-def read_root():
+async def read_root():
     return {"Hello":"World"}
- 
+
+# Renvoyer les données pour toute une filière (gaz/électricite) :
+@app.get("/items")
+def get_filiere_reg(fil: str = None, reg: int = None):
+    print(fil)
+    print(reg)
+    if reg == None : # Ne récupérer que la filière complète si pas de code région
+        da.connexion()
+        data = da.get_filiere(fil = str(fil))
+        da.deconnexion()
+        return data
+
+    elif fil != None and reg != None : # Récupérer la filière pour une région si filière et code région sont renseignés
+        print("coucou")
+        da.connexion()
+        data = da.get_filiere_region(fil = str(fil), reg = int(reg))
+        da.deconnexion()
+        return data
+
+# # Renvoyer les données pour toute une filière (gaz/électricite) et pour une région donnée (code région insee) :
+# @app.get("/items")
+# def get_filiere_region(fil: str, reg:int):
+#     da.connexion()
+#     data = da.get_filiere_region(fil = str(fil), reg = str(reg))
+#     da.deconnexion()
+#     return data
+
 # Supprimer un document particulier :
-@app.delete("/items/{item_id}")
+@app.delete("/items/{item_id}") # fonctionne 
 def del_doc(item_id: str):
     da.connexion()
     da.del_doc(item_id)
     da.deconnexion()
     return {"Document supprimé"}
 
-# Renvoyer les données pour toute une filière (gaz/électricité) :
-@app.get("/items/{q}")
-def get_filiere(q: str):
-    da.connexion()
-    data = da.get_filiere(q)
-    da.deconnexion()
-    return data
 
+# # Run avec auto-reload dès que l'on sauvegarde le code :
+# if __name__ == "__main__":
+#     uvicorn.run("api:app", host="0.0.0.0", port=8000, reload=True)
+
+# Run sans auto-reload
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8500)
--- data.py
+++ data.py
@@ -1,4 +1,4 @@
-from pymongo import MongoClient, DESCENDING
+from pymongo import MongoClient
 import werkzeug
 
 class DataAccess :
@@ -11,6 +11,52 @@ class DataAccess :
     def deconnexion(cls):
         cls.client.close()
 
+    # Récupérer les données pour toute une filière :
+    @classmethod
+    def get_filiere(cls, fil):
+        """Affiche toute la filière Electricité/Gaz selon la query passée en Get dans l'URL à l'api."""
+        fil = str(fil)
+        if fil == "gaz": fil = "Gaz"
+        if fil == "electricite": fil = "Electricité"
+        cur = cls.db.conso.aggregate([
+            { '$match': {
+                "fields.filiere":fil}
+            },
+            { '$unset' : "_id" } # Ne pas prendre les ObjectId
+        ])
+        resultat = [doc for doc in cur]
+
+        if len(resultat) != 0:
+            return resultat
+        else : return {"Les deux filières possibles sont 'gaz' et 'electricite' en minuscules et sans accent."}
+
+    # Récupérer les données pour toute une région
+    @classmethod
+    def get_region(cls, reg):
+        reg = int(reg)
+        resultat = cls.db.conso.aggregate([
+            {'$match': {
+                "fields.code_region": reg}
+            }])
+        return resultat
+
+    @classmethod
+    def get_filiere_region(cls, fil, reg):
+        """Renvoie tous les documents pour une filière ainsi qu'une région donnée (avec son code région INSEE)"""
+        fil = str(fil)
+        if fil == "gaz": fil = "Gaz"
+        if fil == "electricite": fil = "Electricité"
+        reg = int(reg)
+        resultat = cls.db.conso.aggregate([
+                    {'$match': {
+                        "fields.code_region": reg,
+                        "fields.filiere": fil}
+                    },
+                    {"$unset":"_id"}
+                    ])
+        resultat = [r for r in resultat]
+        return resultat
+    
     @classmethod
     def del_doc(cls, id):
         """Supprime un document en base"""
@@ -19,32 +65,16 @@ class DataAccess :
         print(id)
         cls.db.conso.delete_one({"record_id":id})
 
-    @classmethod
-    def get_filiere(cls, q):
-        """Affiche toute la filière Electricité/Gaz selon la query passée en Get dans l'URL à l'api."""
-        q = str(q)
-        if q == "electricite" :
-            cur = cls.db.conso.aggregate([
-                { '$match': {
-                    "fields.filiere":"Electricité"}
-                },
-                { '$unset' : "_id" } # Ne pas prendre les ObjectId
+    # Récupérer la consommation totale pour toute une filière
+    def get_somme_fil(cls, fil):
+        fil = str(fil)
+        if fil == "gaz": fil = "Gaz"
+        if fil == "electricite": fil = "Electricité"
+        result = cls.db.conso.aggregate([
+            {"$match": {"fields.filiere":fil}},
+            {"$group": {"_id":fil,
+                       "conso_totale":{"$sum":"$fields.conso"}}
+            }
             ])
-            resultat = [doc for doc in cur]
-            return resultat
-
-        elif q == "gaz" :
-            cur = cls.db.conso.aggregate([
-                { '$match': {
-                    "fields.filiere":"Gaz"}
-                },
-                { '$unset' : "_id" } # Ne pas prendre les ObjectId
-            ])
-            resultat = [doc for doc in cur]
-            return resultat
-        
-        else :
-            return None
-
-
-        
+        result = [r for r in result]
+        return result
